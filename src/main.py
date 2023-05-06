57a46b5d3e454c338b3343ad294c9b083d2ae089
Ruddi Rodriguez Garcia <rudiscu@gmail.com>
2023-05-07 01:22:45 +0200

    working version 1

---
+++ src/main.py
@@ -0,0 +1,137 @@
+import pandas as pd 
+import streamlit as st
+from googletrans import Translator
+import plotly.graph_objects as go
+from migraine_trace import migraine_trace_figure
+
+translator = Translator()
+
+st.set_page_config(layout="wide")
+@st.cache_data
+def get_data():
+    path ='/Users/ruddirodriguez/Documents/salud/Data/MigraineBuddy_20220923_20230506_1683388430995_-472361220.csv'
+
+    df = pd.read_csv(path,skiprows=2)
+
+    # select only columns with object data type
+    object_cols = df.select_dtypes(include='object')
+
+    for col in object_cols.columns:
+    
+        df[col] = df[col].apply(translator.translate, src='es', dest='en').apply(getattr, args=('text',))
+    return df
+
+df = get_data()
+# define a translation function
+def translate_text(text):
+    return translator.translate(text, dest='en').text
+
+# translate the column names
+translated_cols = [translate_text(col) for col in df.columns]
+
+# create a dictionary of old and new column names
+columns_dict = dict(zip(df.columns, translated_cols))
+#st.write(columns_dict)
+# rename the columns
+df = df.rename(columns=columns_dict)
+
+# get number of days between today and the first date of the data
+
+days = max((pd.to_datetime('today') - pd.to_datetime(df['Started'])).dt.days)
+            
+df.rename(columns={'Started':'Date'}, inplace=True)
+
+col = st.columns(2)
+with col[0]:
+    st.metric ('Number of days', days)
+with col[1]:
+    st.metric ('Number of migraines', len(df))    
+
+
+# Convert the "Date" column to datetime format
+df['Date'] = pd.to_datetime(df['Date'])
+df['Date'] = df['Date'].dt.date
+df['Date'] = pd.to_datetime(df['Date'])
+st.experimental_data_editor(df)
+
+fig_migraine_trace = migraine_trace_figure(df)
+st.plotly_chart(fig_migraine_trace, use_container_width=True)
+
+# Group the data by month and calculate the average pain intensity
+# Create a new column with year and month
+df['YearMonth'] = df['Date'].dt.to_period('W')
+average_intensity_monthly = df.groupby('YearMonth')['Pain intensity'].mean().reset_index()
+std_intensity_monthly = df.groupby('YearMonth')['Pain intensity'].std().reset_index()
+
+# Create a bar trace for the average pain intensity per day
+bar_trace = go.Bar(x=df['Date'], y=df['Pain intensity'],
+                   marker=dict(color='blue'),
+                   name='Daily Average Intensity'
+                   )
+
+# Create a line trace for the average pain intensity per month
+line_trace = go.Scatter(x=average_intensity_monthly['YearMonth'].dt.to_timestamp(), y=average_intensity_monthly['Pain intensity'],
+                        mode='lines',
+                        line=dict(color='red'),
+                        name='Monthly Average Intensity'
+                        )
+
+# Calculate the upper and lower bounds of the error bars
+upper_bound = average_intensity_monthly['Pain intensity'] + std_intensity_monthly['Pain intensity']
+lower_bound = average_intensity_monthly['Pain intensity'] - std_intensity_monthly['Pain intensity']
+
+# Create error bars for the line trace
+line_error_bars = go.Scatter(x=average_intensity_monthly['YearMonth'].dt.to_timestamp(),
+                             y=average_intensity_monthly['Pain intensity'],
+                             mode='lines',
+                             line=dict(color='red', width=0),
+                             error_y=dict(
+                                 type='data',
+                                 symmetric=False,
+                                 array=std_intensity_monthly['Pain intensity'],
+                                 arrayminus=std_intensity_monthly['Pain intensity'],
+                                 color='red',
+                                 thickness=1,
+                                 width=3
+                             ),
+                             name='Monthly Intensity Error',
+                             showlegend=False
+                             )
+
+# Create a layout for the figure
+layout = go.Layout(title='Average Pain Intensity', xaxis=dict(title='Date'), yaxis=dict(title='Average Intensity'))
+
+# Create the figure and add the bar, line, and error bar traces, and layout
+fig = go.Figure(data=[bar_trace, line_trace, line_error_bars], layout=layout)
+
+st.plotly_chart(fig)
+
+
+df ['Possible causes'] = df['Possible causes'].str.lower()
+# Split the values in the "Possible cause" column and stack them into a Series
+causes = df["Possible causes"].str.split(",\s*", expand=True).stack()
+
+# Count the occurrences of each cause
+cause_counts = causes.value_counts()
+
+# Calculate the percentage of occurrence for each cause
+cause_percentages = (cause_counts / cause_counts.sum()) * 100
+
+# Create a bar trace for the cause percentages
+bar_trace = go.Bar(x=cause_percentages.index, y=cause_percentages.values,
+                   marker=dict(color='blue'),
+                   )
+
+# Sort the cause percentages in descending order
+sorted_cause_percentages = cause_percentages.sort_values(ascending=False)
+
+# Create a layout for the figure
+layout = go.Layout(title='Cause Occurrence Percentage', xaxis=dict(title='Possible Cause'), yaxis=dict(title='Percentage'))
+
+# Create the figure and add the bar trace and layout
+fig = go.Figure(data=[bar_trace], layout=layout)
+
+# Sort the x-axis categories based on the cause percentages
+fig.update_xaxes(categoryorder='array', categoryarray=sorted_cause_percentages.index)
+
+st.plotly_chart(fig)
---
+++ src/migraine_trace.py
@@ -0,0 +1,44 @@
+
+import pandas as pd
+import plotly.graph_objects as go
+
+def migraine_trace_figure(df):
+    # Get the minimum and maximum dates from the DataFrame
+    min_date = df['Date'].min()
+    max_date = df['Date'].max()
+
+    # Create a new DataFrame for the days with migraines
+    migraine_dates = pd.date_range(start=min_date, end=max_date, freq='D')
+    migraine_df = pd.DataFrame({'Date': migraine_dates})
+
+    # Merge the migraine DataFrame with the original DataFrame
+    merged_df = pd.merge(migraine_df, df, on='Date', how='left')
+
+    #st.dataframe(merged_df)
+
+    # Create a new column to indicate the presence of migraines
+    merged_df['Migraine'] = merged_df['Date'].apply(lambda x: 1 if x in df['Date'].values else 0)
+
+    # Replace NaN values in the "Pain intensity" column with 0
+    merged_df['Pain intensity'].fillna(0, inplace=True)
+
+    # Create a trace for the migraine events
+    migraine_trace = go.Scatter(x=merged_df['Date'], y=merged_df['Migraine'],
+                                mode='markers',
+                                marker=dict(color=merged_df['Migraine'],
+                                            size=merged_df['Pain intensity']/5,
+                                            colorscale=[[0, 'blue'], [1, 'red']],
+                                            cmin=0, cmax=1,
+                                            sizemode='diameter',
+                                            sizeref=0.1,
+                                            ),
+                                text=merged_df.apply(lambda row: f"Date: {row['Date']}<br>Notes: {row['Grades']}<br>Cause: {row['Possible causes']}", axis=1),
+                                hovertemplate='%{text}<extra></extra>',
+                                )
+
+    # Create a layout for the figure
+    layout = go.Layout(title='Days with Migraines', xaxis=dict(title='Date'), yaxis=dict(title='Migraine'),width=1000, height=250)
+
+    # Create the figure and add the migraine trace and layout
+    fig = go.Figure(data=[migraine_trace], layout=layout)
+    return fig
